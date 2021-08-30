749734c174cbaad1292be31353bfe1f8d44d4a55
Thuha-02 <84179726+Thuha-02@users.noreply.github.com>
2021-08-30 09:16:56 +0700

    fix header

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -23,16 +23,13 @@ class AdminController extends BaseController {
 
     public function post_login() {
         $userObject = new UserModel;
-
         $username = $_POST['username'];
         $password = md5($_POST['password']);
 
         $user = $userObject->where('username', '=', $username, 1);
-        
         if ($user) {
             if ($user['password'] == $password) {
                 $_SESSION['user'] = $user;
-                
                 header("Location: index.php?controller=user");
                 // return $this->view('admin/User/index');
             } else {
